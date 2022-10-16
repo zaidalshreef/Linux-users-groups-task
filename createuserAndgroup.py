47a40250bdf873be757e5b0c9020d785ca3fc06b
zaidalshreef <zaidalshreef@gmail.com>
2022-10-16 10:54:06 +0300

    new file

---
+++ createuserAndgroup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+group_name = sys.argv[1]
+
+with open("userlist.txt") as file:
+    pass
+
+
+
+os.system(f"sudo -S groupadd {group_name}")
+
+users = ''
+for i in range(2, len(sys.argv)):
+    if sys.argv[i] != sys.argv[len(sys.argv)-1]:
+        users += sys.argv[i]+","
+    else:
+        users += sys.argv[i]
+
+os.system(f"sudo -S gpasswd -M {users} {group_name}")
+
+password = input("Enter y/yes to add password to the group: ")
+
+if (password.lower() == "y" or password.lower() == "yes"):
+    os.system(f"sudo -S gpasswd {group_name}")
+
+
+print(users)
