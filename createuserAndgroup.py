ca43df9f040c9b06ac9e2345aa39a207a2fea206
zaidalsharif <zaidalshreef@gmail.com>
2022-10-28 15:12:43 +0300

    new

--- createuserAndgroup.py
+++ createuserAndgroup.py
@@ -14,10 +14,10 @@ with open("userlist.txt","r") as file:
         data = userinfo[0:-1].split(" ")
         name = data[0]
         role = data[1]
+        print(role)
         password = data[2]
         roles = role.split(",")
         commant_role = ",".join(roles)
-        print(commant_role)
         os.system(f"sudo -S useradd {name} -c {commant_role} -e {dataAfter3year}")
         os.system(f"echo {name}:{password} | sudo -S chpasswd")
         for group in roles:
@@ -28,5 +28,4 @@ with open("userlist.txt","r") as file:
       os.system(f"sudo -S chgrp {group_name} ~/{group_name}")
       users_name = ",".join(users)
       os.system(f"sudo -S gpasswd -M {users_name} {group_name}")
-      print (f"{group_name} ")
 
